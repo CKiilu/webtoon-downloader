4deb62f0438fd86df494872c35531bf4afbec47a
CKiilu <cndeti@gmail.com>
2017-06-03 11:04:31 +0300

    Added URL format check

--- input.js
+++ input.js
@@ -3,16 +3,26 @@
 const readline = require('readline-sync');
 const path = require('path');
 const execFile = require('child_process').execFile;
+const fs = require('fs');
+const binaryPath = {
+  default: "node_modules/phantomjs-prebuilt/lib/phantom/bin",
+  alternative: "node_modules/phantomjs-prebuilt/bin"
+};
+const urlFormat = /.*webtoons\.com\/en\/[^\/]+\/[^\/]+\/list\?title_no=[\d]+/;
 
 process.env.PATH += pathAppendFormatted();
 function pathAppendFormatted(){
   return (/^win/.test(process.platform) ? ";" : ":") +
-    path.join(__dirname, "node_modules/phantomjs-prebuilt/lib/phantom/bin");
+    path.join(__dirname, (fs.existsSync(binaryPath.default) ? binaryPath.default : binaryPath.alternative));
 }
 
 (function getInfo(cb) {
   let url, hasRange, range=[];
   url =  readline.question("What comic url would you like to access?\n");
+  while(!urlFormat.test(url)){
+    console.log(`Invalid url entered. \n\tFormat is ${urlFormat}.`);
+    url =  readline.question("What comic url would you like to access?\n");
+  }
   hasRange = readline.question("Would you like to specify a range for the chapters you're viewing?\n");
   if(hasRange === 'y' || hasRange === 'yes'){
     range = readline.question("Set your first and last chapter in the format '1,42'\n").split(",").map(num => parseInt(num));
